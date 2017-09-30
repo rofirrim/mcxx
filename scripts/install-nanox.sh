d07f4a365d978fc2debd3149834653dbf35c914a
Roger Ferrer Ibanez <rofirrim@gmail.com>
2017-09-30 23:38:32 +0100

    Do not override CPP or CCAS

--- scripts/install-nanox.sh
+++ scripts/install-nanox.sh
@@ -4,6 +4,6 @@ set -ex
 wget http://rofi.roger-ferrer.org/apt/mirror/pm.bsc.es/nanox-0.14a.tar.gz
 tar xf nanox-0.14a.tar.gz
 cd nanox-0.14a
-./configure --prefix=$HOME/nanox-install CC=gcc-6 CPP=gcc-6 CCAS=gcc-6 CXX=g++-6
+./configure --prefix=$HOME/nanox-install CC=gcc-6 CXX=g++-6
 make -j$(nproc)
 make install
