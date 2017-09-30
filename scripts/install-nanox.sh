9573e5d18264de8c912f39cc5c048ecbc5784b21
Roger Ferrer Ibanez <rofirrim@gmail.com>
2017-10-01 00:13:50 +0100

    Use ccache in Travis

--- scripts/install-nanox.sh
+++ scripts/install-nanox.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -ex
 
+## This script presumes that ccache is in the PATH
+
 wget http://rofi.roger-ferrer.org/apt/mirror/pm.bsc.es/nanox-0.14a.tar.gz
 tar xf nanox-0.14a.tar.gz
 cd nanox-0.14a
-./configure --prefix=$HOME/nanox-install CC=gcc-6 CXX=g++-6 --disable-debug --disable-instrumentation
+./configure --prefix=$HOME/nanox-install CC="ccache gcc-6" CXX="ccache g++-6" --disable-debug --disable-instrumentation
 make -j$(nproc)
 make install
